e866ade7b12fdd7542647a608f96fcca05052c6a
Carolina Valeriano <carolina.valeriano@atlassoftlabs.com>
2026-06-19 16:43:54 -0300

    fix: update logging configuration to remove file handler and ensure console logging is used

--- config/settings/base.py
+++ config/settings/base.py
@@ -186,37 +186,37 @@ SPECTACULAR_SETTINGS = {
 }
 
 LOGGING = {
-  'version': 1,
-  'disable_existing_loggers': False,
-  'formatters': {
-    'verbose': {
-      'format': '{levelname} {asctime} {module} {message}',
-      'style': '{',
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
     },
-  },
-  'handlers': {
-    'console': {
-      'class':
-        'logging.StreamHandler',
-      'formatter': 
-        'verbose',
+
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
     },
-  },
-  'root': {
-    'handlers': [
-      'console',
-      'file',
-    ],
-    'level': 'INFO',
-  },
-  'loggers': {
-    'django': {
-      'handlers': [
-        'console',
-        'file',
-      ],
-      'level': 'ERROR',
-      'propagate': True,
+
+    'root': {
+        'handlers': [
+            'console',
+        ],
+        'level': 'INFO',
     },
-  },
+
+    'loggers': {
+        'django': {
+            'handlers': [
+                'console',
+            ],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
 }
