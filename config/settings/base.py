2297f477e4843b11195198e21a87de8834a624fe
Carolina Valeriano <carolina.valeriano@atlassoftlabs.com>
2026-06-19 16:21:38 -0300

    fix: move database settings to environment specific configs

--- config/settings/base.py
+++ config/settings/base.py
@@ -78,16 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
